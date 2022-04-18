6d1ae8de8812d08be829e978b3b7ac39380e1787
Hendrik <drikdoank@gmail.com>
2022-04-18 14:16:17 +0700

    product type

---
+++ MySqlDotnetCore/Controllers/ProductTypeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MySqlDotnetCore.Data;
+using MySqlDotnetCore.Models;
+
+namespace MySqlDotnetCore.Controllers
+{
+    public class ProductTypeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        DataContext db = new DataContext();
+
+        public ProductTypeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        [HttpGet]
+
+        public IActionResult Index()
+        {
+            var employeeList = db.ProductTypes.ToList();
+
+            return View(employeeList);
+        }
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public IActionResult Create(ProductType item)
+        {
+
+            if (ModelState.IsValid)
+            {
+                db.Add(item);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Edit(int? id)
+        {
+
+            var item = db.ProductTypes.SingleOrDefault(p => p.seq_Id.Equals(id));
+
+            return View(item);
+        }
+
+        [HttpPost]
+
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(ProductType item)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+                ProductType product = db.ProductTypes.SingleOrDefault(p => p.seq_Id.Equals(item.seq_Id));
+                db.Update(product);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+
+        public IActionResult Delete(int id)
+        {
+            if (ModelState.IsValid)
+            {
+                var item = db.ProductTypes.Find(id);
+                db.ProductTypes.Remove(item);
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
