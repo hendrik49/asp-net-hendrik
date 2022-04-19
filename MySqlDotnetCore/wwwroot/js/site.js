392b7cdcbb8eb987f6633d125aed33a868cad0dc
Hendrik <drikdoank@gmail.com>
2022-04-19 15:27:07 +0700

    remove unused models

--- MySqlDotnetCore/wwwroot/js/site.js
+++ MySqlDotnetCore/wwwroot/js/site.js
@@ -2,3 +2,13 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function() {
+	$('#example').DataTable( {
+		dom: 'Bfrtip',
+		buttons: [
+			'excelHtml5',
+			'pdfHtml5'
+		]
+	} );
+} );
