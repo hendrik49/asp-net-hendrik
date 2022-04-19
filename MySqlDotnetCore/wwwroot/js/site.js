1f90487a0b18ea023caaf7e5853adc1bbc442881
Hendrik <drikdoank@gmail.com>
2022-04-19 16:10:12 +0700

    open pdf

--- MySqlDotnetCore/wwwroot/js/site.js
+++ MySqlDotnetCore/wwwroot/js/site.js
@@ -6,6 +6,12 @@
 $(document).ready(function () {
   $("#example").DataTable({
     dom: "Bfrtip",
-    buttons: ["excelHtml5", "pdfHtml5"],
+    buttons: [
+		"excelHtml5",
+		{
+			extend: 'pdfHtml5',
+			download: 'open'
+		}
+	]
   });
 });
